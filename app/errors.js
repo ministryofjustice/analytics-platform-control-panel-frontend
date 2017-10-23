00efef70f7233d866cb68aaa3f9f5bd0133bbea5
Andy Driver <andy@pagezero.net>
2017-10-23 17:46:38 +0100

    Display internal error messages

--- app/errors.js
+++ app/errors.js
@@ -7,28 +7,8 @@ module.exports = function (err, req, res, next) {
     return next(err);
   }
 
-  if (api_access_denied(err)) {
-    redirect_to_login(req, res, {return_to: req.url});
-
-  } else {
-    show_error(res);
-  }
+  show_error(res, err);
 };
-
-
-function api_access_denied(err) {
-  return err.statusCode && err.statusCode === 403;
-}
-
-
-function redirect_to_login(req, res, options) {
-
-  if (options.return_to) {
-    req.session.returnTo = options.return_to;
-  }
-
-  res.redirect(url_for('base.home'));
-}
 
 
 function show_error(res, err) {

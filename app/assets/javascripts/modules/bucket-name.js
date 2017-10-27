fcf9211b4021af5bfd14503edf9bcc554ad3ef31
Clive Murray <hi+github@clivemurray.com>
2017-10-27 10:44:29 +0100

    Create app template (#19)
    * template and JS for add new app
    
    * Fixes based on feedback

---
+++ app/assets/javascripts/modules/bucket-name.js
@@ -0,0 +1,35 @@
+'use strict';
+
+moj.Modules.bucketName = {
+  inputName: 'new-datasource-name',
+
+  init: function() {
+    var self = this;
+    self.$input = $('#' + self.inputName);
+
+    if (self.$input.length) {
+      self.prefix = self.$input.data('prefix');
+      self.bindEvents();
+    }
+  },
+
+  bindEvents: function() {
+    var self = this;
+
+    self.$input.on('keypress blur', function() {
+      self.formatBucketName();
+    });
+  },
+
+  formatBucketName: function() {
+    var self = this,
+      val = self.$input.val();
+
+    if (val.length < self.prefix.length) {
+      val = self.prefix;
+    }
+
+    val = val.toLowerCase().replace(/ /gi, '-');
+    self.$input.val(val);
+  }
+};

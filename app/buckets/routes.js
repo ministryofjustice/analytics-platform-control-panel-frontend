aa4727b3e99c2577d52eb893f8a7ce5c41caa2ae
Clive Murray <hi+github@clivemurray.com>
2017-10-31 16:35:32 +0000

    Superuser template for listing buckets (#30)
    * superuser template for listing buckets
    
    * fix ensureLoggedIn paths

--- app/buckets/routes.js
+++ app/buckets/routes.js
@@ -3,6 +3,7 @@ var views = require('./views');
 
 module.exports = [
 
+  {name: 'list', pattern: '/buckets', view: views.list_buckets},
   {name: 'new', pattern: '/buckets/new', view: views.new_bucket},
   {name: 'details', pattern: '/buckets/:id', view: views.bucket_details}
 

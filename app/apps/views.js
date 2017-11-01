c12dc332ae2b21413fbfb81ddbdcdf59566f6d5d
axemonkey <hi+github@clivemurray.com>
2017-11-01 16:51:37 +0000

    show real app details from API rather than dummy details

--- app/apps/views.js
+++ app/apps/views.js
@@ -78,6 +78,7 @@ exports.list_apps = [
         res.render('apps/list.html', {
           apps: apps
         });
+
       })
       .catch(next);
   }
@@ -96,58 +97,17 @@ exports.list_user_apps = function (req, res) {
 };
 
 
-exports.app_details = function (req, res) {
+exports.app_details = function (req, res, next) {
 
-  api.get_app(req.params.id).then(function (app) {
+  api.get_app(req.params.id).
+    then(function (app) {
 
-    res.render('apps/details.html', {
-      app: app
-    });
+      res.render('apps/details.html', {
+        app: app
+      });
 
-  }).catch(function (err) {
-
-    var dummyUsers = [
-        {
-          id: 'github|123456',
-          name: 'Harry Pierce',
-          admin: true // probably would be role: <integer> or something?
-        },
-        {
-          id: 'github|234567',
-          name: 'Tom Quinn',
-          admin: false
-        },
-        {
-          id: 'github|345678',
-          name: 'Zoe Reynolds',
-          admin: true
-        }
-      ],
-      dummyBuckets = [
-        {
-          id: 1,
-          name: 'dev-dummy-bucket'
-        }
-      ],
-      app = {
-        name: 'That Dummy App',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nec ligula mollis, vestibulum mi eget, finibus lorem. Nulla ornare velit.',
-        repo_url: 'https://github.com/org-dummy-org/repo-dummy-repo',
-        apps3buckets: [
-          // array of numbers?
-        ],
-        userapps: [
-          // no idea - I'd have expected app users maybe?
-        ],
-        users: dummyUsers,
-        buckets: dummyBuckets
-      };
-
-    res.render('apps/details.html', {
-      app: app
-    });
-
-  });
+    })
+    .catch(next);
 };
 
 

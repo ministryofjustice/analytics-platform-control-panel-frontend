818ecee89b917aefb1320120bcbaecba389c09cc
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-11-15 11:02:46 +0000

    Removed TODO related to "connect_bucket" endpoint
    I added a ticket in Trello:
    
    https://trello.com/c/nvOCtrlY/472-1-rename-appsconnectbucket-appsgrantbucketaccess

--- app/apps/routes.js
+++ app/apps/routes.js
@@ -6,7 +6,6 @@ module.exports = [
   {name: 'new', pattern: '/apps/new', view: views.new_app},
   {name: 'list', pattern: '/apps', view: views.list_apps},
   {name: 'create', method: 'POST', pattern: '/apps/create', view: views.create_app},
-  // TODO: Rename to `connect_bucket` => `grant_bucket_access`
   {name: 'connect_bucket', method: 'POST', pattern: '/apps/:app_id/connect_bucket', view: views.connect_bucket},
   {name: 'list_user_apps', pattern: '/user/:id/apps', view: views.list_user_apps},
   {name: 'details', pattern: '/apps/:id', view: views.app_details},

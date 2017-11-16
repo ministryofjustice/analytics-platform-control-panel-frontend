70b6ba2c4d79a7cd2b5e91ab43562fbf261d2490
Andy Driver <andy@pagezero.net>
2017-11-16 15:37:06 +0000

    Ignore healthcheck requests

--- app/middleware/request-logging.js
+++ app/middleware/request-logging.js
@@ -1,7 +1,9 @@
 module.exports = (app, conf, log) => {
   if (conf.log.requests) {
     log.debug('adding request-logging');
-    return require('morgan')('combined');
+    return require('morgan')('combined', {
+      skip: (req, res) => req.query.healthz !== undefined,
+    });
   }
   return (req, res, next) => { next(); };
 };

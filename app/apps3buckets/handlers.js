3b48f3c9e1813b98eb734ff42df8129a027d2166
Aldo "xoen" Giambelluca <aldo.giambelluca@digital.justice.gov.uk>
2017-11-16 15:06:47 +0000

    Added endpoints to update/delete apps3buckets
    - `POST /apps3buckets/:id` to update access level
    - `POST /apps3buckets/:id/delete` to revoke access
    
    I also updated the template to replace the anchors with forms
    (need styling from Clive).
    
    Manually tested locally, I'll add some automated tests.

--- app/apps3buckets/handlers.js
+++ app/apps3buckets/handlers.js
@@ -22,3 +22,33 @@ exports.create = [
       .catch(next);
   },
 ];
+
+exports.update = [
+  ensureLoggedIn('/login'),
+  (req, res, next) => {
+    const apps3bucket_id = req.params.id;
+    const { access_level, redirect_to } = req.body;
+
+    const apps3bucket = {
+      id: apps3bucket_id,
+      access_level: access_level,
+    };
+
+    api.apps3buckets.update(apps3bucket)
+      .then(() => {
+        res.redirect(redirect_to);
+      })
+      .catch(next);
+  },
+];
+
+exports.delete = [
+  ensureLoggedIn('/login'),
+  (req, res, next) => {
+    api.apps3buckets.delete(req.params.id)
+      .then(() => {
+        res.redirect(req.body.redirect_to);
+      })
+      .catch(next);
+  },
+];

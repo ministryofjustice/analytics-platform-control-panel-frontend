0e8402647c97098db021e7f239591645b87c1ead
axemonkey <hi+github@clivemurray.com>
2017-11-17 10:57:50 +0000

    change confirm message to default, enable specific messages via data attr

--- app/assets/javascripts/modules/confirm.js
+++ app/assets/javascripts/modules/confirm.js
@@ -2,7 +2,7 @@
 
 moj.Modules.jsConfirm = {
   confirmClass: 'js-confirm',
-  confirmMessage: 'Are you sure?',
+  defaultConfirmMessage: 'Are you sure?',
 
   init: function() {
     this.bindEvents();
@@ -12,19 +12,25 @@ moj.Modules.jsConfirm = {
     var self = this;
 
     $('a.' + self.confirmClass).on('click', function(e) {
+      var $el = $(e.target);
       e.preventDefault();
 
-      if (confirm(self.confirmMessage)) {
-        window.document.location = $(e.target).attr('href');
+      if (confirm(self.getConfirmMessage($el))) {
+        window.document.location = $el.attr('href');
       }
     });
 
     $('input[type="submit"].' + self.confirmClass).on('click', function(e) {
+      var $el = $(e.target);
       e.preventDefault();
 
-      if (confirm(self.confirmMessage)) {
-        $(e.target).closest('form').submit();
+      if (confirm(self.getConfirmMessage($el))) {
+        $el.closest('form').submit();
       }
     });
+  },
+
+  getConfirmMessage: function($el) {
+    return $el.data('confirm-message') || this.defaultConfirmMessage;
   }
 };

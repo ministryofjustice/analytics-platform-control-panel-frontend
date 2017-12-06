6115a2f46e6350bd166e991d8b8fa6879ab94470
Andy Driver <andy@pagezero.net>
2017-12-06 15:14:47 +0000

    Share fixtures across tests

--- test/test-k8s-models.js
+++ test/test-k8s-models.js
@@ -3,21 +3,20 @@ const { mock_api } = require('./conftest');
 const { api } = require('../app/k8s-api-client');
 const { ModelSet } = require('../app/base-model');
 const { Pod, Deployment } = require('../app/models');
+const deployments = require('./fixtures/deployments').items;
+const pods_response = require('./fixtures/deployment-pods');
 
 
 describe('K8sModel', () => {
   describe('Deployment', () => {
     it('has a number of pods', () => {
       const ns = 'user-test';
-      const deployments_response = require('./fixtures/apps');
-      const pods_response = require('./fixtures/deployment-pods');
       const expected = new ModelSet(Pod, pods_response.items);
+      const rstudio = new Deployment(deployments[0]);
 
       const pods_request = mock_api()
         .get(`/k8s/api/v1/namespaces/${ns}/pods?labelSelector=app%3Drstudio`)
         .reply(200, pods_response);
-
-      const rstudio = new Deployment(require('./fixtures/deployments').items[0]);
 
       api.namespace = ns;
 
@@ -30,7 +29,8 @@ describe('K8sModel', () => {
 
     it('restarts by deleting all pods', () => {
       const ns = 'user-test';
-      const rstudio = new Deployment(require('./fixtures/deployments').items[0]);
+      const rstudio = new Deployment(deployments[0]);
+
       const delete_pods = mock_api()
         .delete(`/k8s/api/v1/namespaces/${ns}/pods?labelSelector=app%3Drstudio`)
         .reply(200, {

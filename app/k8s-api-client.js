4e572a9fa5311de0cac63f45c550d38d64a36226
Andy Driver <andy@pagezero.net>
2017-12-06 15:08:18 +0000

    Move namespace function to k8s api module and clarify

--- app/k8s-api-client.js
+++ app/k8s-api-client.js
@@ -3,6 +3,23 @@ const { APIClient, JWTAuth } = require('./api-client');
 const url = require('url');
 
 
+function get_namespace(username) {
+  // kubernetes namespaces must be valid DNS names
+  let s = `user-${username.toLowerCase()}`;
+  // may contain only alphanumeric characters and hyphens
+  s = s.replace(/[^a-z0-9]+/, '-');
+  // must start with an alphanumeric character
+  s = s.replace(/^[^a-z0-9]*/, '');
+  // must be at most 63 characters
+  s = s.slice(0, 63);
+  // must end with an alphanumeric character
+  s = s.replace(/[^a-z0-9]*$/, '');
+  return s;
+}
+
+exports.get_namespace = get_namespace;
+
+
 class KubernetesAPIClient extends APIClient {
   endpoint_url(endpoint, namespace = undefined) {
     if (!endpoint) {
--- app/middleware/api.js
+++ app/middleware/api.js
@@ -1,21 +1,11 @@
-function sanitize_username(username) {
-  let s = username.toLowerCase();
-  s = s.replace(/[^a-z0-9]+/, '-');
-  s = s.replace(/^[^a-z0-9]*/, '');
-  s = s.slice(0, 63);
-  s = s.replace(/[^a-z0-9]*$/, '');
-  return s;
-}
-
-
 module.exports = (app, conf, log) => {
   log.info('adding api');
   return (req, res, next) => {
     if (req.user && req.user.id_token) {
       const { api } = require('../api-client');
-      const k8s_api = require('../k8s-api-client').api;
+      const k8s = require('../k8s-api-client');
       api.auth.set_token(req.user.id_token);
-      k8s_api.namespace = `user-${sanitize_username(req.user.username)}`;
+      k8s.api.namespace = k8s.get_namespace(req.user.username);
     }
     next();
   };

2cc1f10d4638406de20e9bd434abe9629b2a0d6f
axemonkey <hi+github@clivemurray.com>
2017-12-06 14:20:39 +0000

    tweak app list page

--- app/users/handlers.js
+++ app/users/handlers.js
@@ -4,7 +4,10 @@ const { App, Bucket, User } = require('../models');
 exports.list_users = (req, res, next) => {
   User.list()
     .then((users) => {
-      res.render('users/list.html', { users });
+      res.render('users/list.html', {
+        users,
+        isSuperuser: req.user.is_superuser,
+      });
     })
     .catch(next);
 };

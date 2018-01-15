65c4b9692f2fa046b28bbff2046dcae54e036a95
Andy Driver <andyhd@users.noreply.github.com>
2018-01-15 11:48:16 +0000

    Tidy up (#110)
    * Tidy up tests
    
    * Move yes_no into template macro
    
    * Show request details on error page
    
    * Format Django errors nicely

--- app/api_clients/control_panel_api.js
+++ app/api_clients/control_panel_api.js
@@ -1,4 +1,4 @@
-const { APIClient } = require('./base');
+const { APIClient, APIError } = require('./base');
 const config = require('../config');
 
 
@@ -11,6 +11,16 @@ class ControlPanelAPIClient extends APIClient {
       header: `JWT ${user.id_token || 'invalid token'}`,
     };
   }
+
+  request(endpoint, { method = 'GET', body = null, params = {} } = {}) {
+    return super.request(endpoint, { method, body, params })
+      .catch((error) => {
+        if (DjangoError.match(error)) {
+          throw new DjangoError(error);
+        }
+        throw error;
+      });
+  }
 }
 
 exports.APIClient = ControlPanelAPIClient;
@@ -18,3 +28,38 @@ exports.APIClient = ControlPanelAPIClient;
 const api = new ControlPanelAPIClient(config.api);
 
 exports.api = api;
+
+
+class DjangoError extends APIError {
+  constructor(error) {
+    super(error);
+    this.original_message = this.message;
+  }
+
+  static match(error) {
+    return error.message && error.message.indexOf('Traceback:') >= 0;
+  }
+
+  get python_traceback() {
+    const pattern = /\\nTraceback:  \\n\\n(.*)\\n\\nException Type/;
+    const match = this.original_message.match(pattern);
+    const traceback = match[1]
+      .replace(/\\n/g, "\n")
+      .replace(/\\"/g, '"')
+      .split(/\n\n/);
+
+    return traceback.map((line) => {
+      const pattern = /^File "([^"]+)" in ([^\n]+)[^0-9]+([0-9]+). ([^\n]+)/;
+      const [ _, file, func, line_no, code ] = line.match(pattern);
+      return { file, func, line_no, code };
+    });
+  }
+
+  get message() {
+    const pattern = /500 - "(.*)\\n\\nRequest Method:/;
+    const match = this.original_message.match(pattern);
+    return match[1].replace(/\\n/g, '\n');
+  }
+}
+
+exports.DjangoError = DjangoError;
--- app/middleware/template-locals.js
+++ app/middleware/template-locals.js
@@ -1,18 +1,3 @@
-/*
-Examples:
-
- - yes_no(is_admin) // => 'Yes' / 'No'
- - yes_no(access_level, 'readwrite') // => 'Yes' / 'No'
- - yes_no(is_admin, true, '(Admin)', '') // => '(Admin)' / ''
- - yes_no(access, 'readwrite', 'Revoke', 'Grant') // => 'Revoke' / 'Grant'
-*/
-function yes_no(value, true_value = true, yes_str = 'Yes', no_str = 'No') {
-  if (value === true_value) {
-    return yes_str;
-  }
-  return no_str;
-}
-
 module.exports = (app, conf, log) => {
   log.info('adding template-locals');
   return (req, res, next) => {
@@ -20,7 +5,6 @@ module.exports = (app, conf, log) => {
     app.locals.current_user = req.user || null;
     app.locals.env = process.env;
     app.locals.req = req;
-    app.locals.yes_no = yes_no;
     next();
   };
 };

b1d3da3ff0b5ddb099a30e5da0cbcef653f69bdd
Andy Driver <andy@pagezero.net>
2018-01-31 14:57:17 +0000

    Display error message when creating app with used repo URL

--- test/apps/test-create.js
+++ test/apps/test-create.js
@@ -1,5 +1,6 @@
 const { assert } = require('chai');
-const { dispatch, mock_api, url_for } = require('../conftest');
+const { config, dispatch, mock_api, url_for, user } = require('../conftest');
+const nock = require('nock');
 const handlers = require('../../app/apps/handlers');
 
 
@@ -7,11 +8,9 @@ describe('apps/create', () => {
   it('creates a new app and redirects to the new app details', () => {
     const form_data = {
       repo_typeahead: 'test-app',
-      description: 'test app description',
       repo_url: 'http://example.com',
     };
     const created_app = { id: 1 };
-    const user = { auth0_id: 'test-user', id_token: 'dummy-token' };
 
     mock_api()
       .post('/apps/')
@@ -26,9 +25,47 @@ describe('apps/create', () => {
         is_admin: true,
       });
 
-    return dispatch(handlers.create, { body: form_data, user })
+    return dispatch(handlers.create, { body: form_data })
       .then(({ redirect_url }) => {
         assert.equal(redirect_url, url_for('apps.details', { id: created_app.id }));
+      });
+  });
+
+  it('shows an error if an app exists for the specified repo', () => {
+    const form_data = {
+      repo_typeahead: 'test-app',
+      repo_url: 'http://example.com',
+    };
+
+    const errors = {
+      repo_url: 'app with this repo url already exists',
+    };
+
+    user.github_access_token = 'test-github-token';
+
+    mock_api()
+      .post('/apps/')
+      .reply(400, errors);
+
+    config.github.orgs = [
+      'test-org-1',
+      'test-org-2',
+    ];
+    config.github.orgs.forEach((org) => {
+      nock(`https://${config.github.host}`)
+        .get(`/orgs/${org}/repos?type=all&page=1&per_page=500`)
+        .reply(200, []);
+    });
+
+    mock_api()
+      .get('/s3buckets/')
+      .reply(200, []);
+
+    return dispatch(handlers.create, { body: form_data })
+      .then(({ redirect_url, template, context }) => {
+        assert.isUndefined(redirect_url);
+        assert.equal(template, 'apps/new.html');
+        assert.deepEqual(context.errors, errors);
       });
   });
 });

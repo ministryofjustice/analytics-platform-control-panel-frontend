c6d206fe978816663249200fd439740c582faf25
Andy Driver <andy@pagezero.net>
2018-02-13 15:00:32 +0000

    Fix bugs

--- test/users/test-delete.js
+++ test/users/test-delete.js
@@ -6,11 +6,12 @@ const handlers = require('../../app/users/handlers');
 describe('users', () => {
   describe('delete', () => {
     it('deletes user and redirects to list', () => {
-      mock_api().delete(`/users/${escape(user.auth0_id)}/`).reply(200, user);
+      const delete_request = mock_api().delete(`/users/${escape(user.auth0_id)}/`).reply(204);
 
       return dispatch(handlers.delete, { params: { id: user.auth0_id } })
         .then(({ redirect_url }) => {
           assert.equal(redirect_url, url_for('users.list'));
+          assert(delete_request.isDone());
         });
     });
   });

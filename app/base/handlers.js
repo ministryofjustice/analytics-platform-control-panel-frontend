95a208f2c41177ff1778f8fd5fb8601c740c20c1
axemonkey <hi+github@clivemurray.com>
2018-03-08 16:30:22 +0000

    Create tab for warehouse buckets and differentiate from webapp buckets

--- app/base/handlers.js
+++ app/base/handlers.js
@@ -19,10 +19,17 @@ exports.home = (req, res, next) => {
   Promise.all([Deployment.list(), User.get(req.user.auth0_id)])
     .then(([tools, user]) => {
       ns.run(() => {
+        const is_warehouse = bucket => !!bucket.s3bucket.is_data_warehouse;
+        const warehouse_buckets = user.users3buckets.filter(bucket => is_warehouse(bucket));
+        const webapp_buckets = user.users3buckets.filter(bucket => !is_warehouse(bucket));
         res.render('base/home.html', {
           tools,
           rstudio_is_deploying,
           user,
+          buckets: {
+            warehouse: warehouse_buckets,
+            webapp: webapp_buckets,
+          },
         });
       });
     })
